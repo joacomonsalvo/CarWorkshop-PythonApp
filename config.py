62be9b4fb3b170f3177ff516639af3e23cdcfc6b
joacomonsalvo <jmonsalvo.business@protonmail.com>
2021-02-14 23:21:48 -0300

    Optimization

--- config.py
+++ config.py
@@ -1,16 +1,18 @@
 import json
 import os
 
-list1 = []
+data_list = []
 USERNAME = os.getlogin()
 PATH = 'C:/Users/{}/Documents/Projects/PYTHON/Vehicles/'.format(USERNAME)
 
 
-def app():
-    start()
-    choose_idiom()
-    country_plate()
-    write_in()
+def start():
+    print("Let's start configuring the app: ")
+    print('Before that you need to accept the terms and conditions of the app\r\n')
+
+    print('TERMS AND CONDITIONS \r\n')
+    #  todo Add terms and conditions
+
 
 
 def choose_idiom():
@@ -21,10 +23,10 @@ def choose_idiom():
 
     if option == 1:
         idiom = 'Spanish'
-        list1.append(idiom)
+        data_list.append(idiom)
     elif option == 2:
         idiom = 'English'
-        list1.append(idiom)
+        data_list.append(idiom)
     else:
         print("That's not an option, try again\r\n")
         choose_idiom()
@@ -38,31 +40,30 @@ def country_plate():
 
     if plate == 1:
         country = 'Argentina'
-        list1.append(country)
+        data_list.append(country)
     elif plate == 2:
         country = 'United States'
-        list1.append(country)
+        data_list.append(country)
     else:
         print("That's not an option, try again")
         country_plate()
 
 
-def start():
-    print("Let's start configuring the app: ")
-    print('Before that you need to accept the terms and conditions of the app\r\n')
-
-    print('TERMS AND CONDITIONS \r\n')
-    #  todo Add terms and conditions
-
-
 def write_in():
     dict0 = {
-        'language': list1[0],
-        'country': list1[1]
+        'language': data_list[0],
+        'country': data_list[1]
     }
 
     with open(PATH + 'data_file.json', 'w') as outfile:
         json.dump(dict0, outfile, indent=2)
+
+
+def app():
+    start()
+    choose_idiom()
+    country_plate()
+    write_in()
 
 
 app()
