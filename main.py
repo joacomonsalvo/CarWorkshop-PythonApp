1d7d83abdc82e85c9791b4fff96a88a2cfb78130
joacomonsalvo <jmonsalvo.business@protonmail.com>
2021-02-21 20:23:09 -0300

    Visual Interface Demo 0.0.3

--- main.py
+++ main.py
@@ -3,19 +3,43 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-def windows(xpos, ypos, width, hight):
+
+class MyWindow(QMainWindow):
+    "Big class"
+
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Vehicles App")
+        self.init_ui()
+
+    def init_ui(self):
+        "Fuck DocString"
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText('My first Label')
+        self.label.move(100, 100)
+
+        #Create button
+        self.button = QtWidgets.QPushButton(self)
+        self.button.setText("Click me")
+        self.button.clicked.connect(self.click)
+
+    def click(self):
+        "Click"
+        self.label.setText("You pressed the button")
+        self.update()
+
+
+    def update(self):
+        "Update Size"
+        self.label.adjustSize()
+
+
+def windows():
     "Window"
     app = QApplication(sys.argv) # Config set up for qt app
-    win = QMainWindow() # Create the window/widget that is shown
-    win.setGeometry(xpos, ypos, width, hight) # Set dimensions
-    win.setWindowTitle('Vehicles App') # Title that the windows shows at the top
-
-    #Label
-    label = QtWidgets.QLabel(win)
-    label.setText('My first Label')
-    label.move(100, 100)
-
+    win = MyWindow()
     win.show() #Show the window
     sys.exit(app.exec_()) # Make cross bottom work
 
-windows(200, 200, 300, 300)
+windows()
