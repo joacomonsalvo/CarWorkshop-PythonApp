59f98545e76e5c090b9c019cb87d8bcf4863601c
joacomonsalvo <jmonsalvo.business@protonmail.com>
2021-02-21 18:28:47 -0300

    errors solved

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-# consoleprogram.py but with the visual interface
-
+"Native Modules"
+import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
-
-xpos = 200
-ypos = 200
-width = 300
-hight = 300
 
 def windows(xpos, ypos, width, hight):
+    "Window"
     app = QApplication(sys.argv) # Config set up for qt app
     win = QMainWindow() # Create the window/widget that is shown
     win.setGeometry(xpos, ypos, width, hight) # Set dimensions
@@ -23,4 +18,4 @@ def windows(xpos, ypos, width, hight):
     win.show() #Show the window
     sys.exit(app.exec_()) # Make cross bottom work
 
-windows(xpos, ypos, width, hight)
+windows(200, 200, 300, 300)
