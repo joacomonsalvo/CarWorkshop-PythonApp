ae20c6a49ba057bdc7832b0bf5d81caab0fd5e62
joacomonsalvo <jmonsalvo.business@protonmail.com>
2021-03-04 19:34:36 -0300

    UI Working

--- main.py
+++ main.py
@@ -1,44 +1,69 @@
-"Native Modules"
-import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MyWindow(QMainWindow):
-    "Big class"
+class Ui_MainWindow:
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
 
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        self.setGeometry(200, 200, 300, 300)
-        self.setWindowTitle("Vehicles App")
-        self.init_ui()
+        "Label"
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(140, 140, 381, 241))
+        
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        
+        self.label.setFont(font)
+        self.label.setObjectName("label")
 
-    def init_ui(self):
-        "Create Label and button1"
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText('My first Label')
-        self.label.move(100, 100)
+        MainWindow.setCentralWidget(self.centralwidget)
+        
+        "MenuBar"
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName('menubar')
 
-        #Create button
-        self.button = QtWidgets.QPushButton(self)
-        self.button.setText("Click me")
-        self.button.clicked.connect(self.click)
+        "File"
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName('menuFile')
 
-    def click(self):
-        "Click action"
-        self.label.setText("You pressed the button")
-        self.update()
+        MainWindow.setMenuBar(self.menubar)
 
-    def update(self):
-        "Update Size"
-        self.label.adjustSize()
+        "StatusBar"
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName('statusbar')
+        MainWindow.setStatusBar(self.statusbar)
 
+        "Shortcuts"
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        self.actionSave.setShortcutContext(QtCore.Qt.WindowShortcut)
+        self.actionSave.setObjectName('actionSave')
 
-def windows():
-    "Window"
-    app = QApplication(sys.argv) # Config set up for qt app
-    win = MyWindow()
-    win.show()
+        self.menuFile.addAction(self.actionSave)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate('MainWindow', 'CarWorkshop'))
+        self.label.setText(_translate("CarWorkshop", "TextLabel"))
+        self.menuFile.setTitle(_translate('MainWindow', 'File'))
+        
+        self.actionSave.setText(_translate('MainWindow', 'Save'))
+        self.actionSave.setStatusTip(_translate('MainWindow', 'Save a file'))
+        self.actionSave.setShortcut(_translate('MainWindow', 'Ctrl+S'))
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv) # Config set up for qt app
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_()) # Make cross bottom work
-
-windows()
